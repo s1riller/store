094153c91e966fe82d7ea5890362bbec2f287b53
Saveliy <miheev.saveliy.2002@gmail.com>
2023-03-27 01:10:06 +0800

    Добавил главную страницу и страницу с продуктами

--- store-server/store/tutorial/views.py
+++ store-server/store/tutorial/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
-    return render(request,'tutorial/test.html') #Чтобы использовать заготовку нужно указать место до этой заготовки
+    return render(request, 'tutorial/test.html', {
+        'title': 'Test Template'})  # Чтобы использовать заготовку нужно указать место до этой заготовки, после передаются данные на форму
